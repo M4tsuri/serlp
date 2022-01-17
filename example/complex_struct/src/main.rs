7ec664b96373ae73381f5bb69db2db9be4fdfcf8
M4tsuti <ctsinon@outlook.com>
2022-01-17 19:56:20 +0800

    added an example

--- example/complex_struct/src/main.rs
+++ example/complex_struct/src/main.rs
@@ -1,23 +1,3 @@
-## A (de)serializer for RLP encoding in ETH
-
-### Cargo.toml
-
-```
-serde_rlp = 0.1.0
-serde = { version = "1.0.133", features = ['derive'] }
-```
-
-### Not Supportted Types 
-
-- bool
-- float numbers
-- enum
-
-### Example code
-
-You can find more examples [here](https://github.com/M4tsuri/serde_rlp/tree/main/example)
-
-```rust
 use serde_rlp::{de::from_bytes, ser::to_bytes};
 use serde::{Serialize, Deserialize};
 use serde_bytes;
@@ -26,15 +6,14 @@ use serde_bytes;
 struct Third<T> {
     inner: T
 }
-
 #[derive(Serialize, Debug, PartialEq, Eq, Deserialize)]
+
 struct Embeding<'a> {
     tag: &'a str,
     ed: Embedded,
     #[serde(with = "serde_bytes")]
     bytes: Vec<u8>
 }
-
 #[derive(Serialize, Debug, PartialEq, Eq, Deserialize)]
 struct Embedded {
     time: u64,
@@ -62,24 +41,3 @@ fn main() {
 
     assert_eq!(origin, embed);
 }
-```
-
-### Design principle
-
-Accroding to the ETH Yellow Paper, all supported data structure can be represented with either recursive list of byte arrays $\mathbb{L}$ or byte arrays $\mathbb{B}$. So we can transform all Rust's compound types, for example, tuple, struct and list, into lists. And then encode them as exactly described in the paper
-
-For example, the structure in example code, can be internally treated as the following form:
-
-```
-[
-    "This is a tooooooooooooo loooooooooooooooooooong tag", [
-        114514, 
-        [191, -9810], 
-        [
-            [[], [[]], [[], [[]]]]
-        ]
-    ], 
-    "哼.啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊"
-]
-```
-
